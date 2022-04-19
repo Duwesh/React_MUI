b244cc570b8a8d5563ca721e1e86ed3bc71a1085
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-04-19 20:32:18 +0530

    buttons variation and prop added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { Typography } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
+import { MuiTypography } from "./components/MuiTypography";
+import { MuiButton } from "./components/MuiButton";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      {/* <MuiTypography /> */}
+      <MuiButton />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ vite-project/src/components/MuiTypography.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+//Typography
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">Masai School</Typography>
+      <Typography variant="h2">Masai School</Typography>
+      <Typography variant="h3">Masai School</Typography>
+      <Typography variant="h4">Masai School</Typography>
+      <Typography variant="h5">Masai School</Typography>
+      <Typography variant="h6">Masai School</Typography>
+
+      <Typography variant="subtitle1">School</Typography>
+
+      <Typography variant="subtitle2">School</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque,
+        expedita voluptatem earum impedit non error veritatis laboriosam eveniet
+        facilis saepe perferendis, quaerat ipsum magnam? Exercitationem sunt
+        quae iste? Cupiditate, vel?
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque,
+        expedita voluptatem earum impedit non error veritatis laboriosam eveniet
+        facilis saepe perferendis, quaerat ipsum magnam? Exercitationem sunt
+        quae iste? Cupiditate, vel?
+      </Typography>
+    </div>
+  );
+};
